0254ed280f0d7430e9810b20146e13f0f4e288c6
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-23 22:09:49 +0800

    Commit 2

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,3 +1,7 @@
 print("~~~~~~~~~~ALGORITHM - SELECTION SORT~~~~~~~~~~")
 print("~~~~~~~~~~Donasco , Venus M.~~~~~~~~~~")
 print("~~~~~~~~~~BSCOE 2-2~~~~~~~~~~")
+
+#Python - Selection Sort
+
+def SelectionSort(array, size):
