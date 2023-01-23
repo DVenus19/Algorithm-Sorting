02460adbc8d633f4d8ce66e301f8ce665e83ab27
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-23 23:43:24 +0800

    Commit 9

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -8,3 +8,6 @@ def BubbleSort(array):
     for i in range(len(array)): #To access the given array element
         for j in range(0, len(array) - i - 1): #To compare given the array element
             if array[j] > array[j + 1]: #to sort in descending order ,change > to <
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp #if elements are not in the intended order swap the elements
