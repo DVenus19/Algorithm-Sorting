dc51588acc6fc8588ba03d59fda3554b124dc2f8
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-23 23:37:53 +0800

    Commit 7

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -5,4 +5,5 @@ print("~~~~~~~~~~BSCOE 2-2~~~~~~~~~~")
 #Python - Bubble Sort
 
 def BubbleSort(array):
-    for i in range(len(array)): #To access the array element
+    for i in range(len(array)): #To access the given array element
+        for j in range(0, len(array) - i - 1): #To compare given the array element
