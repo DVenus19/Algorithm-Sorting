c230c180498b70125136779b815047e3829ae5bd
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-24 21:03:54 +0800

    Commit 6

--- Merge Sort.py
+++ Merge Sort.py
@@ -6,6 +6,7 @@ print("~~~~~~~~~~BSCOE 2-2~~~~~~~~~~")
 def MergeSort(array):
     if len(array) > 1:
 
+#This where r represents the array being divide into 2 subarrays
         r = len(array)//2
         X = array[:r]
         Y = array[r:]
