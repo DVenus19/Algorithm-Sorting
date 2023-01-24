619172da2e8513e25827a969898fd21442f30cb6
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-24 22:40:23 +0800

    Commit 7

--- Quick Sort.py
+++ Quick Sort.py
@@ -7,7 +7,8 @@ def Partition(array, low, high):
     pivot = array[high]
     i = low - 1
     for j in range(low, high):
-        if array[j] <= pivot:
+        if array[j] <= pivot:# if element smaller than pivot is found it will swap with the greater element pointed by i
             i = i + 1
 
         (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
