6c82295cf95b61559c169be653875b1117bbc45b
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-01-24 19:28:38 +0800

    Commmit 8

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -10,6 +10,13 @@ def Insertion_Sort(array):
         j = i - 1
         while j >= 0 and value < array[j]:
             array[j+1] = array[j]
+            j -= 1
+            array[j+1] = value
+            return array
+
+array = [50,71,7,81,20,6,30,90,86,10]
+print()
+
 
 
 
